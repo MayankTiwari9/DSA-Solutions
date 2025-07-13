c6a29d35f9c44e9d3b5a742ffca55fec34b476d7
MayankTiwari9 <mt1027319@gmail.com>
2025-07-13 11:38:32 +0530

    Reverse Array

---
+++ Apna College/Arrays/Easy/ReverseAnArray/Solution.java
@@ -0,0 +1,38 @@
+package ReverseAnArray;
+
+public class Solution {
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 3, 2, 6, 5 };
+        // int arr[] = { 4, 5, 2 };
+        // int arr[] = {1};
+
+        int res[] = reverseArray(arr);
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
+
+    static int[] reverseArray(int arr[]) {
+        int n = arr.length;
+
+        // Brute force
+        // int temp[] = new int[n];
+
+        // for (int i = 0; i < n; i++) {
+        //     temp[n - 1 - i] = arr[i];
+        // }
+
+        // return temp;
+
+        // Optimised
+
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+
+        return arr;
+    }
+}
---
+++ Apna College/Arrays/Easy/ReverseAnArray/Solution.js
@@ -0,0 +1,28 @@
+// let arr = [ 1, 4, 3, 2, 6, 5 ];
+let arr = [4, 5, 2];
+// let arr = [1];
+
+function solution(arr) {
+  let n = arr.length;
+
+  // Brute Force
+  //   let temp = [];
+
+  //   for (let i = 0; i < n; i++) {
+  //     temp[i] = arr[n - 1 - i];
+  //   }
+
+  //   return temp;
+
+  // Optimised
+
+  for (let i = 0; i < n / 2; i++) {
+    let temp = arr[i];
+    arr[i] = arr[n - i - 1];
+    arr[n - i - 1] = temp;
+  }
+
+  return arr;
+}
+
+console.log(solution(arr));
